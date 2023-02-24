bc5d4401bba7758dffdb63075233f11fdc34cd9a
FilipRazek <65445960+FilipRazek@users.noreply.github.com>
2023-02-24 10:57:09 +0100

    :recycle: Create mask comparison factory

--- helpers/comparers/compareHighestFullHouse.ts
+++ helpers/comparers/compareHighestFullHouse.ts
@@ -1,26 +1,3 @@
-import { CardCount, ComparisonResult } from "../types";
-import getHighestCardsFromMask from "./getHighestCardsFromMask";
+import maskComparisonFactory from "./maskComparisonFactory";
 
-export default (hand1: CardCount, hand2: CardCount): ComparisonResult => {
-  const mask = [3, 2];
-  const hand1HighestCards = getHighestCardsFromMask(hand1, mask);
-  const hand2HighestCards = getHighestCardsFromMask(hand2, mask);
-
-  if (!hand1HighestCards) {
-    if (!hand2HighestCards) {
-      return 0;
-    }
-    return -1;
-  }
-  if (!hand2HighestCards) {
-    return 1;
-  }
-  for (let i = 0; i < mask.length; i++) {
-    if (hand1HighestCards[i] > hand2HighestCards[i]) {
-      return 1;
-    } else if (hand2HighestCards[i] > hand1HighestCards[i]) {
-      return -1;
-    }
-  }
-  return 0;
-};
+export default maskComparisonFactory([3, 2]);
---
+++ helpers/comparers/maskComparisonFactory.ts
@@ -0,0 +1,17 @@
+import { CardCount, ComparisonResult } from "../types";
+import getHighestCardsFromMask from "./getHighestCardsFromMask";
+
+export default (mask: number[]) =>
+  (hand1: CardCount, hand2: CardCount): ComparisonResult => {
+    const hand1HighestCards = getHighestCardsFromMask(hand1, mask);
+    const hand2HighestCards = getHighestCardsFromMask(hand2, mask);
+
+    if (!hand1HighestCards) return hand2HighestCards ? -1 : 0;
+    if (!hand2HighestCards) return 1;
+
+    for (let i = 0; i < mask.length; i++) {
+      if (hand1HighestCards[i] > hand2HighestCards[i]) return 1;
+      if (hand2HighestCards[i] > hand1HighestCards[i]) return -1;
+    }
+    return 0;
+  };
